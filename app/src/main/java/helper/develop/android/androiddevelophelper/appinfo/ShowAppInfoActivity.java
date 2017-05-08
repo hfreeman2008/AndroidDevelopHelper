fb43269c6b7f34cbf920d19ac28da39a00fd7035
xiaoming.he <xiaoming.he@tinno.com>
2017-05-08 09:34:55 +0800

    adjust ui

--- app/src/main/java/helper/develop/android/androiddevelophelper/appinfo/ShowAppInfoActivity.java
+++ app/src/main/java/helper/develop/android/androiddevelophelper/appinfo/ShowAppInfoActivity.java
@@ -4,12 +4,15 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import helper.develop.android.androiddevelophelper.R;
 
 public class ShowAppInfoActivity extends AppCompatActivity {
 
+    private Button button = null;
     private TextView showAppInfo = null;
     private StringBuilder sb;
     private static final int show_app_info_cm = 1;
@@ -32,15 +35,25 @@ public class ShowAppInfoActivity extends AppCompatActivity {
     }
 
     private void init() {
-        showAppInfo = (TextView) findViewById(R.id.showAppInfo);
-        showAppInfo.setText(getString(R.string.beginCollectAppInfo));
-        handler.post(new Runnable() {
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                sb = ShowAppInfo.show(ShowAppInfoActivity.this);
-                Toast.makeText(ShowAppInfoActivity.this, getString(R.string.show_app_info_ok), Toast.LENGTH_LONG).show();
-                handler.sendEmptyMessage(show_app_info_cm);
+            public void onClick(View v) {
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        sb = ShowAppInfo.show(ShowAppInfoActivity.this);
+                        Toast.makeText(ShowAppInfoActivity.this,
+                                getString(R.string.show_app_info_ok),
+                                Toast.LENGTH_LONG).show();
+                        handler.sendEmptyMessage(show_app_info_cm);
+                    }
+                });
+
             }
         });
+        showAppInfo = (TextView) findViewById(R.id.showAppInfo);
+        showAppInfo.setText(getString(R.string.beginCollectAppInfo));
     }
 }
